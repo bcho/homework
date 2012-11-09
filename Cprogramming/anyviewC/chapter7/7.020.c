f15391d60ecb1b79ca26141c952401d0f1e26026
hbc <bcxxxxxx@gmail.com>
2012-11-09 11:01:05 +0800

    fixed 7.020

--- Cprogramming/anyviewC/chapter7/7.020.c
+++ Cprogramming/anyviewC/chapter7/7.020.c
@@ -4,22 +4,30 @@ int change(int n);
 
 int main()
 {
-    printf("%d", change(40));
+    printf("%d", change(10));
 
     return 0;
 }
 
 int change(int n)
 {
-    int ret;
+    int ret, times[3] = {n / 10, n / 5, n / 1}, i, j, k;
 
-    // 10 = 1 * 10 = 10 * 1 = 5 * 2 = 5 * 1 + 1 * 5 (4)
-    // 5 = 5 * 1 = 1 * 5                            (2)
-    // 1 = 1 * 1                                    (1)
-    if (n < 5)
-        ret = 1;
-    else
-        ret = n / 10 * 4 + n % 10 / 5 * 2;
+    if (!n) return 0;
+
+    // I have no idea about how to solve it quicker...
+    // so, why not search every solutions...
+    // it only takes O(n^3 / 50).
+    // yeah, you may say it's not a fast algorithm,
+    // but I think the data is small (most are smaller than 100),
+    // so we may get O(n^2) ...
+    for (ret = 0, i = 0;i <= times[0];i++) {
+        for (j = 0;j <= times[1];j++) {
+            for (k = 0;k <= times[2];k++)
+                if (i * 10 + j * 5 + k * 1 == n)
+                    ret++;
+        }
+    }
 
     return ret;
 }
