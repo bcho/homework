f02e13b27d1b343b9b94442dafa28cb35cafd569
hbc <bcxxxxxx@gmail.com>
2012-11-22 22:28:21 +0800

    added binary trees

---
+++ data_structures/binary_trees/binary_trees.h
@@ -0,0 +1,25 @@
+#ifndef BINARY_TREES
+#define BINARY_TREES
+
+typedef char element_t;
+struct binary_tree_r {
+    element_t value;
+    struct binary_tree_r *left, *right;
+};
+typedef struct binary_tree_r *binary_tree_t;
+
+/* assumes that: i is parent, i * 2 and i * 2 + 1 are children,
+ * and BLANK_NODE means this node is not exist.
+ * */
+binary_tree_t create_tree(char nodes[], int length);
+void dispose_tree(binary_tree_t t);
+
+int tree_height(binary_tree_t t);
+
+void preorder_traversal(binary_tree_t t, void (*visit)(binary_tree_t t));
+void inorder_traversal(binary_tree_t t, void (*visit)(binary_tree_t t));
+void postorder_traversal(binary_tree_t t, void (*visit)(binary_tree_t t));
+
+#define MAX_TREE_NODE 1000
+#define BLANK_NODE 0
+#endif
---
+++ data_structures/binary_trees/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+#define sep printf("\n")
+
+void print_node(binary_tree_t t);
+
+int main()
+{
+    char msg[] = " deadbeef";
+    binary_tree_t t = create_tree(msg, 8);
+
+    printf("%d", tree_height(t));
+    sep;
+
+    preorder_traversal(t, print_node);
+    sep;
+    postorder_traversal(t, print_node);
+    sep;
+    inorder_traversal(t, print_node);
+
+    return 0;
+}
+
+void print_node(binary_tree_t t)
+{
+    printf("%c ", t->value);
+}
