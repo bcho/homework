bfca99bbd318064667a81df6d99106a2624e545e
hbc <bcxxxxxx@gmail.com>
2013-07-02 23:12:04 +0800

    added simple daytime check

---
+++ unix-nw/daytimetcp.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define MAXLINE 1024
+#define SERVER_PORT 5816
+#define BACKLOG 15
+
+int
+main()
+{
+    int serverfd, clientfd;
+    int opt;
+    struct sockaddr_in serveraddr, clientaddr;
+
+    serverfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverfd < 0)
+        perror("socket");
+    opt = 1;
+    setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    bzero(&serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(SERVER_PORT);
+
+    if (bind(serverfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)))
+        perror("bind");
+
+    if (listen(serverfd, BACKLOG) < 0)
+        perror("listen");
+
+    for (;;) {
+        socklen_t size = sizeof(clientaddr);
+        char buff[MAXLINE];
+        time_t ticks;
+
+        clientfd = accept(serverfd, (struct sockaddr *) &clientaddr, &size);
+        if (clientfd < 0)
+            perror("accept");
+
+        printf("connect from %s, port %d\n",
+                inet_ntop(AF_INET, &clientaddr.sin_addr, buff, sizeof(buff)),
+                ntohs(clientaddr.sin_port));
+
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+        write(clientfd, buff, strlen(buff));
+
+        close(clientfd);
+    }
+
+    return 0;
+}
