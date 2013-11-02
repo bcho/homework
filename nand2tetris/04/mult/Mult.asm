2e24d52371180c4d873f6d9964bafc85ae40813d
hbc <bcxxxxxx@gmail.com>
2013-11-02 15:19:18 +0800

    added multi

--- nand2tetris/04/mult/Mult.asm
+++ nand2tetris/04/mult/Mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+
+// init R2
+@R2
+M = 0
+
+(LOOP)
+// check if R1 reaches 0
+@R1
+D = M
+@END
+D;JEQ
+
+// add R0 to R2
+@R0
+D = M
+@R2
+M = M + D
+
+// dec R1
+@R1
+M = M - 1
+
+// next loop
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
