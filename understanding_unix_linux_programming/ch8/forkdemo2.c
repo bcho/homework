0cfd9aec3c7ec8b1ea4567511390166ab319a06d
hbc <bcxxxxxx@gmail.com>
2013-12-13 23:34:38 +0800

    Added a demo

---
+++ understanding_unix_linux_programming/ch8/forkdemo2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("my pid is %d\n", getpid());
+
+    fork();
+    fork();
+    fork();
+
+    printf("my pid is %d\n", getpid());
+    
+    return 0;
+}
