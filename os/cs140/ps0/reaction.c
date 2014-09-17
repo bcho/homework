712ee91365ba179493e3dbf55c27773c19afe08a
hbc <bcxxxxxx@gmail.com>
2014-09-17 19:56:26 +0800

    (os: cs140): add reaction.
    我真不会写多线程 ~_~

--- os/cs140/ps0/reaction.c
+++ os/cs140/ps0/reaction.c
@@ -6,23 +6,53 @@
 void make_water();
 
 struct reaction {
-	// FILL ME IN
+    int h, o;
+    struct lock lock;
+    struct condition has_new_h, has_enough_o;
 };
 
 void
 reaction_init(struct reaction *reaction)
 {
-	// FILL ME IN
+    reaction->h = 0;
+    reaction->o = 0;
+    lock_init(&reaction->lock);
+    cond_init(&reaction->has_new_h);
+    cond_init(&reaction->has_enough_o);
 }
 
 void
 reaction_h(struct reaction *reaction)
 {
-	// FILL ME IN
+    lock_acquire(&reaction->lock);
+    reaction->h += 1;
+
+    cond_signal(&reaction->has_new_h, &reaction->lock);
+
+    // Waiting for an O to exit. 
+    cond_wait(&reaction->has_enough_o, &reaction->lock);
+
+    lock_release(&reaction->lock);
 }
 
 void
 reaction_o(struct reaction *reaction)
 {
-	// FILL ME IN
+    lock_acquire(&reaction->lock);
+    reaction->o += 1;
+
+    // Wait until there are enough H.
+    while (reaction->h < 2)
+        cond_wait(&reaction->has_new_h, &reaction->lock);
+
+    // Notify 2 H to exit.
+    cond_signal(&reaction->has_enough_o, &reaction->lock);
+    cond_signal(&reaction->has_enough_o, &reaction->lock);
+
+    // Make water.
+    reaction->h -= 2;
+    reaction->o -= 1;
+    make_water();
+
+    lock_release(&reaction->lock);
 }
