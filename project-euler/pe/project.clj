431b471b14472334746f0ff3819e08d39700d70c
hbc <bcxxxxxx@gmail.com>
2014-10-05 22:14:11 +0800

    (project-euler): introduce lein exec to run scripts.

---
+++ project-euler/pe/project.clj
@@ -0,0 +1,7 @@
+(defproject hbc.project-euler "0.1.0-SNAPSHOT"
+  :description "Project Euler"
+  :url "http://github.com/bcho/homework"
+
+  :plugins [[lein-exec "0.3.4"]]
+
+  :dependencies [[org.clojure/clojure "1.6.0"]])
