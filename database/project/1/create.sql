def0d240107008775e0232540a2cd6a4adcece55
hbc <bcxxxxxx@gmail.com>
2014-11-24 13:15:47 +0800

    (database: project 1): add some fixtures.

---
+++ database/project/1/create.sql
@@ -0,0 +1,89 @@
+create table department (
+    dno char(6),
+    dept_name char(20) not null,
+    header char(8),
+
+    primary key(dno)
+);
+
+create table speciality (
+    spno char(8),
+    dno char(6) not null,
+    spname char(20) not null,
+
+    primary key(spno)
+    foreign key(dno) references department(dno)
+);
+
+create table student (
+    s_no char(8),
+    sname char(8) not null,
+    sex char(2),
+    sbirthday smalldatetime,
+    dno char(6),
+    spno char(8),
+    class_no char(4),
+
+    primary key(s_no),
+    foreign key(dno) references department(dno),
+    foreign key(spno) references speciality(spno)
+);
+
+create table course (
+    cno char(10),
+    cname char(20) not null,
+    spno char(8),
+    ctno tinyint,       /* 0 for 必修，1 for 选修 */
+    lecture tinyint,
+    experiment tinyint,
+    semester tinyint,
+    credit tinyint,
+
+    primary key(cno),
+    foreign key(spno) references speciality(spno)
+);
+
+create table student_course (
+    s_no char(8),
+    tcid smallint,          /* references course? */
+    score tinyint,
+
+    primary key(s_no, tcid),
+    foreign key(s_no) references student(s_no) on delete cascade
+);
+
+create table teacher (
+    t_no char(8),
+    t_name char(8) not null,
+    t_sex char(2),
+    t_birthday smalldatetime,
+    dno char(6),
+    teach_title char(10),
+
+    primary key(t_no),
+    foreign key(dno) references department(dno)
+);
+
+create table teacher_course (
+    tcid smallint,
+    t_no char(8),
+    spno char(8),
+    class_no char(4),
+    cno char(10) not null,
+    semester char(6),
+    schoolyear char(10),
+
+    primary key(tcid),
+    foreign key(t_no) references teacher(t_no),
+    foreign key(spno) references speciality(spno),
+    foreign key(cno) references course(cno)
+);
+
+create table class (
+    spno char(8),
+    class_no char(4),
+    header char(8),
+
+    primary key(spno, class_no),
+    foreign key(spno) references speciality(spno)
+);
---
+++ database/project/1/insert.sql
@@ -0,0 +1,95 @@
+/* department */
+insert into department (dno, dept_name, header)
+values ('ce', 'Computer', 'Mr. CS');
+
+insert into department (dno, dept_name, header)
+values ('in', 'Information', 'Mr. Info');
+
+insert into department (dno, dept_name, header)
+values ('en', 'English', 'Mrs. Eng');
+
+insert into department (dno, dept_name, header)
+values ('py', 'Physical', 'Mr. Phy');
+
+insert into department (dno, dept_name, header)
+values ('jp', 'Japaness', 'Mr. Jap');
+
+/* speciality */
+insert into speciality (spno, dno, spname)
+values ('cs', 'ce', 'Computer Science');
+
+insert into speciality (spno, dno, spname)
+values ('se', 'ce', 'Software Engineering');
+
+insert into speciality (spno, dno, spname)
+values ('ne', 'ce', 'Network Engineering');
+
+insert into speciality (spno, dno, spname)
+values ('dm', 'ce', 'Digital Media');
+
+insert into speciality (spno, dno, spname)
+values ('be', 'en', 'Business English');
+
+/* class */
+insert into class (spno, class_no, header)
+values ('cs', '1201', 'Mr. CS 01');
+
+insert into class (spno, class_no, header)
+values ('cs', '1202', 'Mr. CS 02');
+
+insert into class (spno, class_no, header)
+values ('se', '1201', 'Mr. SE 01');
+
+insert into class (spno, class_no, header)
+values ('se', '1201', 'Mr. SE 01');
+
+insert into class (spno, class_no, header)
+values ('ne', '1201', 'Mr. NE 01');
+
+/* student */
+insert into student (s_no, sname, sex, sbirthday, dno, spno, class_no)
+values ('120001', 'alex', 'M', '1993-01-01', 'ce', 'cs', '1202');
+
+insert into student (s_no, sname, sex, sbirthday, dno, spno, class_no)
+values ('120002', 'amy', 'F', '1993-02-01', 'ce', 'cs', '1202');
+
+insert into student (s_no, sname, sex, sbirthday, dno, spno, class_no)
+values ('120003', 'bob', 'M', '1993-03-01', 'ce', 'se', '1201');
+
+insert into student (s_no, sname, sex, sbirthday, dno, spno, class_no)
+values ('120004', 'daisy', 'F', '1993-04-01', 'ce', 'se', '1201');
+
+insert into student (s_no, sname, sex, sbirthday, dno, spno, class_no)
+values ('120005', 'alex', 'M', '1993-05-01', 'ce', 'ne', '1201');
+
+/* course */
+insert into course (cno, cname, spno, ctno, lecture, experiment, semester, credit)
+values ('cs01', 'C Programming', 'cs', 0, 42, 30, 1, 2);
+
+insert into course (cno, cname, spno, ctno, lecture, experiment, semester, credit)
+values ('cs02', 'Data Structure', 'cs', 0, 42, 30, 2, 2);
+
+insert into course (cno, cname, spno, ctno, lecture, experiment, semester, credit)
+values ('cs03', 'Operating System', 'cs', 0, 30, 10, 1, 1);
+
+insert into course (cno, cname, spno, ctno, lecture, experiment, semester, credit)
+values ('cs04', 'Database', 'cs', 0, 30, 10, 1, 1);
+
+insert into course (cno, cname, spno, ctno, lecture, experiment, semester, credit)
+values ('cs05', 'Data Mining', 'cs', 1, 20, 0, 2, 1);
+
+/* teacher */
+insert into teacher (t_no, t_name, t_sex, t_birthday, dno, teach_title)
+values ('t01', 'Mr. t01', 'M', '1980-01-01', 'ce', 'Proferssor');
+
+insert into teacher (t_no, t_name, t_sex, t_birthday, dno, teach_title)
+values ('t02', 'Ms. t02', 'F', '1985-02-01', 'ce', 'Lecturer');
+
+insert into teacher (t_no, t_name, t_sex, t_birthday, dno, teach_title)
+values ('t03', 'Mr. t03', 'M', '1975-01-01', 'ce', 'Professor');
+
+insert into teacher (t_no, t_name, t_sex, t_birthday, dno, teach_title)
+values ('t04', 'Ms. t04', 'F', '1965-02-01', 'ce', 'Professor');
+
+insert into teacher (t_no, t_name, t_sex, t_birthday, dno, teach_title)
+values ('t05', 'Mr. t05', 'M', '1983-01-01', 'ce', 'Lecturer');
