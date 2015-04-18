49d68c2c4083df2d5a5de7e0108fffd7f73615ac
hbc <bcxxxxxx@gmail.com>
2015-04-19 04:40:50 +0800

    (crypto): add basic frontend app.

--- crypto/hw3/watch.py
+++ crypto/hw3/watch.py
@@ -3,7 +3,8 @@ from livereload import Server, shell
 server = Server()
 
 
-server.watch('./*.html', shell('make'))
+server.watch('./*.html')
+server.watch('./*.css')
 server.watch('./*.coffee', shell('make'))
 
 server.serve()
