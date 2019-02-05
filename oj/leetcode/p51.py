180c4f26011951297cd970ed7337fc66aae070d9
hbc <bcxxxxxx@gmail.com>
2019-02-05 14:34:36 +0800

    feat(leetcode): add solution

---
+++ oj/leetcode/p51.py
@@ -0,0 +1,53 @@
+class Solution:
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        if n < 1:
+            return []
+
+        def check_cross(q, qs):
+            for dx, dy in [(1, 1), (1, -1), (-1, 1), (-1, -1)]:
+                x, y = q
+                while True:
+                    x, y = x + dx, y + dy
+                    if x >= n or x < 0 or y >= n or y < 0:
+                        break
+                    if (x, y) in qs:
+                        return False
+            return True
+
+        rv = []
+
+        def perm(qs, ds):
+            if len(qs) == n:
+                for q in qs:
+                    if not check_cross(q, qs):
+                        return
+                qs_sorted = sorted(qs, key=lambda x: x[1])
+                m = []
+                for x, _ in qs_sorted:
+                    r = '.' * x + 'Q' + '.' * (n - x - 1)
+                    m.append(r)
+                rv.append(m)
+                return
+            for idx, d in enumerate(ds):
+                q = (d, n - len(ds))
+                qs.add(q)
+                perm(qs, ds[:idx] + ds[idx+1:])
+                qs.remove(q)
+
+        perm(set(), [i for i in range(n)])
+
+        return rv
+
+
+s = Solution()
+
+print(s.solveNQueens(1))
+print(s.solveNQueens(2))
+print(s.solveNQueens(3))
+print(s.solveNQueens(4))
+print(s.solveNQueens(5))
+print(s.solveNQueens(8))
