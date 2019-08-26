e15eda04cfe194452000732bc60e030645e7a1d7
hbc <bcxxxxxx@gmail.com>
2019-08-26 12:26:38 +0800

    feat(cs231n): add softmax

--- cs231n/assignment1/cs231n/classifiers/softmax.py
+++ cs231n/assignment1/cs231n/classifiers/softmax.py
@@ -33,7 +33,26 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+
+    for i in range(num_train):
+        scores = X[i].dot(W)
+        scores -= scores.max()
+        scores_expsum = np.sum(np.exp(scores))
+        cor_ex = np.exp(scores[y[i]])
+        loss += - np.log(cor_ex / scores_expsum)
+
+        dW[:, y[i]] += (-1) * (scores_expsum - cor_ex) / scores_expsum * X[i]
+        for j in range(num_classes):
+            if j == y[i]:
+                continue
+            dW[:, j] += np.exp(scores[j]) / scores_expsum * X[i]
+
+    loss /= num_train
+    loss += reg * np.sum(W * W)
+    dW /= num_train
+    dW += 2 * reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -58,7 +77,21 @@ def softmax_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_train = X.shape[0]
+
+    scores = X.dot(W)
+    scores -= scores.max()
+    scores = np.exp(scores)
+    scores_sums = np.sum(scores, axis=1)
+    cors = scores[range(num_train), y]
+    loss = cors / scores_sums
+    loss = -np.sum(np.log(loss)) / num_train + reg * np.sum(W * W)
+
+    s = np.divide(scores, scores_sums.reshape(num_train, 1))
+    s[range(num_train), y] = - (scores_sums - cors) / scores_sums
+    dW = X.T.dot(s)
+    dW /= num_train
+    dW += 2 * reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
