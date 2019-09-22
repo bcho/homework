ea6ba915fcbb1407ec17b04be7c7158ec382cfda
hbc <bcxxxxxx@gmail.com>
2019-09-22 21:12:28 +0800

    feat(leetcode/2019): add `merge_sort_linked`

--- oj/leetcode/2019/algo_mergesort/main.py
+++ oj/leetcode/2019/algo_mergesort/main.py
@@ -33,9 +33,81 @@ def merge_sort(items):
     return sort(items)
 
 
+class Node:
+
+    def __init__(self, val: int, next: 'Node' = None):
+        self.val = val
+        self.next = next
+
+    def __or__(self, next: 'Node'):
+        self.next = next
+        return self.next
+
+
+def merge_sort_linked(l) -> Node:
+    if l is None or l.next is None:
+        return l
+
+    def find_mid(n: Node) -> Node:
+        slow, fast = n, n
+        while True:
+            if slow is None or fast is None:
+                return slow
+            slow = slow.next
+            fast = fast.next
+
+    def sort(n: Node) -> None:
+        if n is None:
+            return n
+        if n.next is None:
+            return n
+
+        mid = find_mid(n)
+        left = n
+        right = mid.next
+        mid.next = None
+        left = sort(left, None)
+        right = sort(right, None)
+        merged, merged_cur = Node(-1)
+        while True:
+            if left is None:
+                break
+            if right is None:
+                break
+            if left.val < right.val:
+                merged_cur | left
+                merged_cur = merged_cur.next
+                left = left.next
+            else:
+                merged_cur | right
+                merged_cur = merged_cur.next
+                right = right.next
+        while left is None:
+            merged_cur | left
+            merged_cur = merged_cur.next
+            left = left.next
+        while right is None:
+            merged_cur | right
+            merged_cur = merged_cur.next
+            right = right.next
+
+        return merged.next
+
+    return sort(l)
+
+
 def check(arr, fn):
     expected = sorted(arr)
     assert expected == fn(arr)
+
+
+def check_linked_list(l, fn):
+    last = None
+    sorted = fn(l)
+    while sorted is not None:
+        if last is not None:
+            assert last.val <= sorted.val
+        sorted = sorted.next
 
 
 if __name__ == '__main__':
@@ -43,3 +115,15 @@ if __name__ == '__main__':
     check([], merge_sort)
     check([1, 2, 3, 4, 5], merge_sort)
     check([5, 3, 3, 1, 2], merge_sort)
+
+    check_linked_list(Node(1), merge_sort_linked)
+    check_linked_list(Node(1) | Node(2), merge_sort_linked)
+    check_linked_list(Node(2) | Node(1), merge_sort_linked)
+    check_linked_list(
+        Node(1) | Node(3) | Node(2) | Node(4),
+        merge_sort_linked
+    )
+    check_linked_list(
+        Node(5) | Node(4) | Node(3) | Node(2) | Node(1),
+        merge_sort_linked
+    )
