35fb2a4b13720b8f28321a6ab7582c98f67c9a23
hbc <bcxxxxxx@gmail.com>
2019-10-04 12:26:42 +0800

    feat(leetcode/2019): add state propagation solution for `target-sum`

--- oj/leetcode/2019/target-sum/main.py
+++ oj/leetcode/2019/target-sum/main.py
@@ -29,3 +29,28 @@ class Solution:
                 f[t] += f[t-num]
 
         return f[sum_p]
+
+    def findTargetSumWays_state(self, nums: List[int], S: int) -> int:
+        from collections import defaultdict
+
+        if len(nums) < 1:
+            return 0
+        if len(nums) == 1 and nums[0] == S:
+            return 1 if S != 0 else 2
+
+        # idea: for each item, we can put +/- sign, so this item will affect
+        # next item's count. We use f[k, v] to represent the total count
+        # for the value v, using item #k, so:
+        #
+        #   # propagate to next state
+        #   f[n + 1, v + a[n]] += f[n]
+        #   f[n + 1, v - a[n]] += f[n]
+
+        f = [defaultdict(int) for i in range(len(nums) + 1)]
+        f[0][0] = 1
+        for idx, num in enumerate(nums):
+            for sum_by_far, count in f[idx].items():
+                f[idx + 1][sum_by_far + num] += count
+                f[idx + 1][sum_by_far - num] += count
+
+        return f[len(nums)][S]
